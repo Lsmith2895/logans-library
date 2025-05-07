c1fa65203add0aacd4a4ed275cf16d8091192e64
Logan Smith <lsmith2895@yahoo.com>
2025-05-07 17:29:11 -0400

    create context, provider, and reducer

---
+++ src/context/BookContext.tsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useReducer, type Dispatch, type ReactNode } from "react";
+import type { GoogleBookResponse } from "../types/book";
+
+type State = {
+    rawBook: GoogleBookResponse | null;
+    searchTerm: string; // this will be the ISBN that the user passes in 10 or 13 digit number as a string
+}
+
+type Action = 
+{ type: 'SET_BOOK'; payload: GoogleBookResponse | null} 
+| { type: 'SET_SEARCH'; payload: string}
+
+const BookContext = createContext<{
+    state: State, 
+    dispatch: Dispatch<Action>;
+} | null>(null);
+
+function reducer(state: State, action: Action): State {
+    switch (action.type) {
+        case "SET_BOOK":
+            return {...state, rawBook: action.payload};
+        case "SET_SEARCH":
+            return {...state, searchTerm: action.payload}
+        default:
+            return state
+    }
+}
+
+function BookProvider({children}: {children: ReactNode}) {
+    const [state, dispatch ] = useReducer(reducer, {rawBook: null, searchTerm: ''});
+
+    return (
+        <BookContext.Provider value={{state, dispatch}}>
+            {children}
+        </BookContext.Provider>
+    )
+}
+
+function useBookContext(){
+    const context = useContext(BookContext);
+    if(!context){
+        throw new Error('useBookContext must be used in Book Provider')
+    }
+    return context
+}
+
+export { BookProvider, useBookContext}
