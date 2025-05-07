be8ea2e0a56cf4d4e662e7ded1e4cb15389602bb
Logan Smith <lsmith2895@yahoo.com>
2025-05-07 15:58:30 -0400

    use new book type

--- src/api/googleBooks.ts
+++ src/api/googleBooks.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
+import type { GoogleBookResponse } from "../types/book";
 
 
-async function getBookByISBN(isbn: string) {
+async function getBookByISBN(isbn: string): Promise<GoogleBookResponse | null> {
     const apiKey = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
     const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
     
--- src/pages/RawDataPage.tsx
+++ src/pages/RawDataPage.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
 import { getBookByISBN } from "../api/googleBooks"
+import type { GoogleBookResponse } from "../types/book"
 
 function RawDataPage(){
     //TODO pass this in later
     const isbn = '9781501184161'
-    const [book, setBook] = useState(null)
+    const [book, setBook] = useState<GoogleBookResponse | null>(null)
 
     useEffect( () => {
         const fetchData = async () => {
