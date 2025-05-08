900bf0950a164af8d976d47a8245d6200958ef7b
Logan Smith <lsmith2895@yahoo.com>
2025-05-08 15:06:49 -0400

    update raw data page to no longer show null but a how to guid for the app

--- src/pages/RawDataPage.tsx
+++ src/pages/RawDataPage.tsx
@@ -3,9 +3,12 @@ import { useBookContext } from "../context/BookContext"
 function RawDataPage(){
     const { state } = useBookContext();
     return (
-        <div className="pt-10 whitespace-pre-wrap">
+        <>
+        {state.rawBook ? (<div className="pt-10 whitespace-pre-wrap">
             {JSON.stringify(state.rawBook, null, 2)}
-        </div>
+        </div>) : (<div>home page how to guide</div>)}
+        
+        </>
     )
 }
 
