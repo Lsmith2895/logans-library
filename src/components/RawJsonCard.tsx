8ba47532fa34b345ad711ffa310de54da13a69aa
Logan Smith <lsmith2895@yahoo.com>
2025-05-09 13:09:43 -0400

    link raw json preview to raw json page

--- src/components/RawJsonCard.tsx
+++ src/components/RawJsonCard.tsx
@@ -1,28 +1,31 @@
 import { Editor } from '@monaco-editor/react';
 import { useBookContext } from '../context/BookContext';
+import { Link } from 'react-router-dom';
 
 function RawJsonCard() {
   const { state } = useBookContext();
 
   return (
-    <div className="mx-4 my-4 h-96 max-w-full rounded-3xl bg-gray-950 p-6 sm:mx-6">
-      <h1 className="p-3 text-4xl">Raw JSON</h1>
-      <div className="h-5/6 pt-5">
-        <Editor
-          theme="vs-dark"
-          language="json"
-          defaultValue={JSON.stringify(state.rawBook, null, 2)}
-          options={{
-            minimap: { enabled: false },
-            stickyScroll: { enabled: false },
-            fontSize: 12,
-            wordWrap: 'on',
-            scrollBeyondLastLine: false,
-            lineNumbers: 'off',
-          }}
-        />
+    <Link to="/raw">
+      <div className="mx-4 my-4 h-96 max-w-full rounded-3xl bg-gray-950 p-6 sm:mx-6">
+        <h1 className="p-3 text-4xl">Raw JSON</h1>
+        <div className="h-5/6 pt-5">
+          <Editor
+            theme="vs-dark"
+            language="json"
+            defaultValue={JSON.stringify(state.rawBook, null, 2)}
+            options={{
+              minimap: { enabled: false },
+              stickyScroll: { enabled: false },
+              fontSize: 12,
+              wordWrap: 'on',
+              scrollBeyondLastLine: false,
+              lineNumbers: 'off',
+            }}
+          />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
