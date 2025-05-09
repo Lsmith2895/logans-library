e45e505afb19d8eaede0367917b352eba53bc6e1
Logan Smith <lsmith2895@yahoo.com>
2025-05-09 11:18:03 -0400

    lint

--- src/pages/RawDataPage.tsx
+++ src/pages/RawDataPage.tsx
@@ -3,7 +3,7 @@ import { useBookContext } from '../context/BookContext';
 function RawDataPage() {
   const { state } = useBookContext();
   return (
-    <div className='p-4'>
+    <div className="p-4">
       {state.rawBook ? (
         <div className="pt-10 whitespace-pre-wrap">{JSON.stringify(state.rawBook, null, 2)}</div>
       ) : (
