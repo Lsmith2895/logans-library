dbaad606e8bffe73631f07d22ab7145e98f22f70
Logan Smith <lsmith2895@yahoo.com>
2025-05-09 20:49:00 -0400

    fix breakpoint syntax

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { RawJsonCard } from '../components/RawJsonCard';
 
 function HomePage() {
   return (
-    <div className="max-w-7xl p-4 md: px-20">
+    <div className="max-w-7xl p-4 md:px-20">
       <div className='mb-10 p-6'>
         <div className='gradient-text text-5xl sm:text-7xl font-black p-4'> Question<br />Everything</div>
         <div className='text-3xl ml-3.5 sm:text-4xl'>Discover books worth changing your mind for.</div>
