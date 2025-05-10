57492fa731b2536d69b0523c02df782c4847c3f9
Logan Smith <lsmith2895@yahoo.com>
2025-05-09 20:49:33 -0400

    lint & format

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,9 +4,16 @@ import { RawJsonCard } from '../components/RawJsonCard';
 function HomePage() {
   return (
     <div className="max-w-7xl p-4 md:px-20">
-      <div className='mb-10 p-6'>
-        <div className='gradient-text text-5xl sm:text-7xl font-black p-4'> Question<br />Everything</div>
-        <div className='text-3xl ml-3.5 sm:text-4xl'>Discover books worth changing your mind for.</div>
+      <div className="mb-10 p-6">
+        <div className="gradient-text p-4 text-5xl font-black sm:text-7xl">
+          {' '}
+          Question
+          <br />
+          Everything
+        </div>
+        <div className="ml-3.5 text-3xl sm:text-4xl">
+          Discover books worth changing your mind for.
+        </div>
       </div>
       <RawJsonCard />
       <HowToGuide />
