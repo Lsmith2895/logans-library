05676d98f4e4450801f6223eb1fc0b348c9b1134
Logan Smith <lsmith2895@yahoo.com>
2025-05-10 09:23:32 -0400

    rename how to card to fix dev confusion

--- src/components/HowToCard.tsx
+++ src/components/HowToCard.tsx
@@ -1,4 +1,6 @@
-function HowToGuide() {
+import { Link } from 'react-router-dom';
+
+function HowToCard() {
   return (
     <div className="card-gradient flex h-96 h-[400px] max-w-full rounded-3xl p-4 sm:mx-6">
       <div className="py-7 md:py-14">
@@ -7,13 +9,15 @@ function HowToGuide() {
           {' '}
           Learn how to search for books by ISBN <br /> with these simle steps.
         </p>
-        <button className="mt-4 rounded-3xl border-2 border-[#b4e300] bg-[#b4e300] px-6 py-2 font-extrabold text-black">
-          Learn
-        </button>
+        <Link to={'/guide'}>
+          <button className="mt-4 rounded-3xl border-2 border-[#b4e300] bg-[#b4e300] px-6 py-2 font-extrabold text-black">
+            Learn
+          </button>
+        </Link>
       </div>
       <img src="./public/book-icon.png" className="my-14 h-30 md:h-40" />
     </div>
   );
 }
 
-export { HowToGuide };
+export { HowToCard };
