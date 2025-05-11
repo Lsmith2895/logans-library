eaa5ba6fd193c775bedac14709e818ee6fbc5a25
Logan Smith <lsmith2895@yahoo.com>
2025-05-10 21:01:37 -0400

    add test ids to mobile menu due to testing complexities

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -25,7 +25,7 @@ function NavBar() {
   return (
     <nav className="mb-4 flex h-20 items-center justify-between border-b bg-black p-4 px-10 text-[#b4e300]">
       <div className="flex items-center">
-        <img src="../public/SquareLogo.png" className="w-10" aria-label="logo" />
+        <img src="../public/SquareLogo.png" className="w-10" aria-label="logo" alt="logo" />
         <Link to="/">
           <div className="text-2xl font-extrabold text-white md:text-4xl">Fallibilism</div>
         </Link>
@@ -37,12 +37,18 @@ function NavBar() {
           className="focus:outline-none"
           onClick={() => setMenuOpen(prev => !prev)}
           type="button"
+          data-testid="hamburger-button"
         >
           <img src="../public/menu.svg" className="h-6 w-6" />
           {/* Mobile Menu */}
           {menuOpen && (
             <div className="absolute right-0 mt-4 flex w-36 flex-col rounded-2xl border border-gray-700 bg-black p-4 text-lg md:hidden">
-              <Link to="/raw" onClick={() => setMenuOpen(false)} className="p-2">
+              <Link
+                data-testid="hamburger-raw-book"
+                to="/raw"
+                onClick={() => setMenuOpen(false)}
+                className="p-2"
+              >
                 Raw Book
               </Link>
               <Link to="/library" className="p-2" onClick={() => setMenuOpen(false)}>
