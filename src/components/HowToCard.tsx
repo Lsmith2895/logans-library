0603bfb4ac3b3be66e004735d835d8ba51e945f8
Logan Smith <lsmith2895@yahoo.com>
2025-05-11 11:54:25 -0400

    remove height attr not needed

--- src/components/HowToCard.tsx
+++ src/components/HowToCard.tsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 
 function HowToCard() {
   return (
-    <div className="card-gradient my-9 flex h-[300px] max-w-full rounded-3xl p-4 md:my-28 md:h-[400px]">
+    <div className="card-gradient my-9 flex  max-w-full rounded-3xl p-4 md:my-28">
       <div className="w-2/3 py-2 md:px-6 md:py-14">
         <div className="card-title text-4xl font-black md:text-7xl"> How-To Guide</div>
         <p className="pt-2 md:pt-8 md:text-3xl">
-          Learn how to search for books by ISBN with these simle steps.
+          Learn how to search for books by ISBN with these simple steps.
         </p>
         <Link to={'/guide'}>
           <button className="mt-6 rounded-3xl border-2 border-[#b4e300] bg-[#b4e300] px-6 py-2 font-extrabold text-black">
