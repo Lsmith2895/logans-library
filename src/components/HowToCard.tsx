f57edec879fb681562e0dcf09565e84a96e7788f
Logan Smith <lsmith2895@yahoo.com>
2025-05-11 11:47:20 -0400

    make how to card great on mobile and desktop (breathing room)

--- src/components/HowToCard.tsx
+++ src/components/HowToCard.tsx
@@ -2,19 +2,21 @@ import { Link } from 'react-router-dom';
 
 function HowToCard() {
   return (
-    <div className="card-gradient mt-9 flex h-[300px] max-w-full rounded-3xl p-4">
-      <div className="py-7 md:py-14">
-        <div className="card-title text-4xl font-black md:text-5xl"> How-To Guide</div>
-        <p>
-          Learn how to search for books by ISBN <br /> with these simle steps.
+    <div className="card-gradient my-9 flex h-[300px] max-w-full rounded-3xl p-4 md:my-28 md:h-[400px]">
+      <div className="w-2/3 py-2 md:px-6 md:py-14">
+        <div className="card-title text-4xl font-black md:text-7xl"> How-To Guide</div>
+        <p className="pt-2 md:pt-8 md:text-3xl">
+          Learn how to search for books by ISBN with these simle steps.
         </p>
         <Link to={'/guide'}>
-          <button className="mt-4 rounded-3xl border-2 border-[#b4e300] bg-[#b4e300] px-6 py-2 font-extrabold text-black">
-            Learn
+          <button className="mt-6 rounded-3xl border-2 border-[#b4e300] bg-[#b4e300] px-6 py-2 font-extrabold text-black">
+            Start Searching
           </button>
         </Link>
       </div>
-      <img src="./public/book-icon.png" className="my-14 h-30 md:h-40" />
+      <div className="w-1/3">
+        <img src="./public/book-icon.png" className="my-12 h-30 md:h-50" />
+      </div>
     </div>
   );
 }
